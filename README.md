16818d3b642e46f090c89b1d091c2777a6ef1c28
Chris Ferdinandi <hello@gomakethings.com>
2014-01-27 09:39:28 -0500

    Switched to a data attribute for the toggle selector (separates scripts from styles).
    Also…
      * Removed unneeded `.save-form-data` and `.delete-form-data` classes.
      * Replace `.hide-no-js` with `.form-saver` wrapper class.
      * Updated code structure for better modularity.
      * Prefixed script with a `;` to prevent errors if other scripts are
    incorrectly closed.
      * Added namespacing to IIFE.

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ A simple script that lets users save and reuse form data.
 Getting started with Form Saver is really easy. [View the online tutorial](http://cferdinandi.github.io/form-saver/) or dig through the `index.html` file.
 
 ## Changelog
+* v3.0 (January 27, 2014)
+  * Switched to a data attribute for the toggle selector (separates scripts from styles).
+  * Removed unneeded `.save-form-data` and `.delete-form-data` classes.
+  * Replace `.hide-no-js` with `.form-saver` wrapper class.
+  * Prefixed script with a `;` to prevent errors if other scripts are incorrectly closed.
+  * Added namespacing to IIFE.
 * v2.2 (December 6, 2013)
   * Added Sass support.
 * v2.1 (November 21, 2013)
--- js/form-saver.js
+++ js/form-saver.js
@@ -1,6 +1,6 @@
 /* =============================================================
 
-	Form Saver v2.2
+	Form Saver v3.0
 	A simple script that lets users save and reuse form data.
 	http://gomakethings.com
 
@@ -9,123 +9,184 @@
 
  * ============================================================= */
 
-(function (window, document, undefined) {
+;window.formSaveBtnr = (function (window, document, undefined) {
 
 	'use strict';
 
 	if ( 'querySelector' in document && 'addEventListener' in window && 'localStorage' in window && Array.prototype.forEach ) {
 
-		// Variables
+		// SELECTORS
+
 		var forms = document.forms;
-		var formSave = document.querySelectorAll('.form-save-data');
-		var formRemove = document.querySelectorAll('.form-delete-data');
+		var formSaveBtn = document.querySelectorAll('[data-form-save]');
+		var formRemoveBtn = document.querySelectorAll('[data-form-delete]');
 
-		// Save form data
-		[].forEach.call(formSave, function (save) {
-			save.addEventListener('click', function(e) {
 
-				e.preventDefault();
+		// METHODS
 
-				// Variables
-				var form = save.form;
-				var formSaverID = form.id === null || form.id === '' ? 'formsaver-' + document.URL : 'formsaver-' + form.id;
-				var formSaverData = {};
-				var formFields = form.elements;
-				var formStatus = form.querySelectorAll('.form-status');
+		// Save form data to localStorage
+		var saveForm = function (btn) {
 
-				// Add field data to array
-				[].forEach.call(formFields, function (field) {
-					if ( !buoy.hasClass(field, 'form-no-save') ) {
-						if ( field.type == 'radio' || field.type == 'checkbox' ) {
-							if ( field.checked === true ) {
-								formSaverData[field.name + field.value] = 'on';
-							}
-						} else if ( field.type != 'hidden' && field.type != 'submit' ) {
-							if ( field.value !== null && field.value !== '' ) {
-								formSaverData[field.name] = field.value;
-							}
+			// SELECTORS
+
+			var form = btn.form;
+			var formSaveBtnrID = form.id === null || form.id === '' ? 'formSaveBtnr-' + document.URL : 'formSaveBtnr-' + form.id;
+			var formSaveBtnrData = {};
+			var formFields = form.elements;
+			var formStatus = form.querySelectorAll('.form-status');
+
+
+			// METHODS
+
+			// Convert field data into an array
+			var prepareField = function (field) {
+				if ( !buoy.hasClass(field, 'form-no-save') ) {
+					if ( field.type == 'radio' || field.type == 'checkbox' ) {
+						if ( field.checked === true ) {
+							formSaveBtnrData[field.name + field.value] = 'on';
+						}
+					} else if ( field.type != 'hidden' && field.type != 'submit' ) {
+						if ( field.value !== null && field.value !== '' ) {
+							formSaveBtnrData[field.name] = field.value;
 						}
 					}
-				});
-
-				// Display save success message
-				[].forEach.call(formStatus, function (status) {
-					if ( save.getAttribute('data-message') === null ) {
-						status.innerHTML = '<div>Saved!</div>';
-					} else {
-						status.innerHTML = '<div>' + save.getAttribute('data-message') + '</div>';
-					}
-				});
-
-				// Save form data in localStorage
-				localStorage.setItem( formSaverID, JSON.stringify(formSaverData) );
-
-				// If no form ID is provided, generate friendly console message encouraging one to be added
-				if ( form.id === null || form.id === '' ) {
-					console.log('FORM SAVER WARNING: This form has no ID attribute. This can create conflicts if more than one form is included on a page, or if the URL changes or includes a query string or hash value.');
 				}
+			};
 
-			}, false);
-		});
+			// Display status message
+			var displayStatus = function (status ) {
+				if ( btn.getAttribute('data-message') === null ) {
+					status.innerHTML = '<div>Saved!</div>';
+				} else {
+					status.innerHTML = '<div>' + btn.getAttribute('data-message') + '</div>';
+				}
+			};
 
-		// Delete form data
-		[].forEach.call(formRemove, function (remove) {
-			remove.addEventListener('click', function(e) {
 
-				e.preventDefault();
+			// EVENTS, LISTENERS, AND INITS
 
-				// Variables
-				var form = remove.form;
-				var formSaverID = form.id === null || form.id === '' ? 'formsaver-' + document.URL : 'formsaver-' + form.id;
-				var formStatus = form.querySelectorAll('.form-status');
-				var formMessage = remove.getAttribute('data-message') === null ? '<div>Deleted!</div>' : '<div>' + remove.getAttribute('data-message') + '</div>';
+			// Add field data to array
+			[].forEach.call(formFields, function (field) {
+				prepareField(field);
+			});
 
-				// Remove form data
-				localStorage.removeItem(formSaverID);
+			// Display save success message
+			[].forEach.call(formStatus, function (status) {
+				displayStatus(status);
+			});
 
-				// Display delete success message
-				if ( remove.getAttribute('data-clear') == 'true' ) {
-					sessionStorage.setItem(formSaverID + '-formSaverMessage', formMessage);
+			// Save form data in localStorage
+			localStorage.setItem( formSaveBtnrID, JSON.stringify(formSaveBtnrData) );
+
+			// If no form ID is provided, generate friendly console message encouraging one to be added
+			if ( form.id === null || form.id === '' ) {
+				console.log('FORM SAVER WARNING: This form has no ID attribute. This can create conflicts if more than one form is included on a page, or if the URL changes or includes a query string or hash value.');
+			}
+
+		};
+
+		// Remove form data from localStorage
+		var deleteForm = function (btn) {
+
+			// SELECTORS
+
+			var form = btn.form;
+			var formSaveBtnrID = form.id === null || form.id === '' ? 'formSaveBtnr-' + document.URL : 'formSaveBtnr-' + form.id;
+			var formStatus = form.querySelectorAll('.form-status');
+			var formMessage = btn.getAttribute('data-message') === null ? '<div>Deleted!</div>' : '<div>' + btn.getAttribute('data-message') + '</div>';
+
+
+			// METHODS
+
+			var displayStatus = function () {
+				if ( btn.getAttribute('data-clear') == 'true' ) {
+					sessionStorage.setItem(formSaveBtnrID + '-formSaveBtnrMessage', formMessage);
 					location.reload(false);
 				} else {
 					[].forEach.call(formStatus, function (status) {
 						status.innerHTML = formMessage;
 					});
 				}
+			};
 
+
+			// EVENTS, LISTENERS, AND INITS
+
+			// Remove form data
+			localStorage.removeItem(formSaveBtnrID);
+
+			// Display delete success message
+			displayStatus(btn);
+
+		};
+
+		// Load form data from localStorage
+		var loadForm = function (form) {
+
+			// SELECTORS
+
+			var formSaveBtnrID = form.id === null || form.id === '' ? 'formSaveBtnr-' + document.URL : 'formSaveBtnr-' + form.id;
+			var formSaveBtnrData = JSON.parse( localStorage.getItem(formSaveBtnrID) );
+			var formFields = form.elements;
+			var formStatus = form.querySelectorAll('.form-status');
+
+
+			// METHODS
+
+			var populateField = function (field) {
+				if ( formSaveBtnrData !== null ) {
+					if ( field.type == 'radio' || field.type == 'checkbox' ) {
+						if ( formSaveBtnrData[field.name + field.value] == 'on' ) {
+							field.checked = true;
+						}
+					} else if ( field.type != 'hidden' && field.type != 'submit' ) {
+						if ( formSaveBtnrData[field.name] !== null && formSaveBtnrData[field.name] !== undefined ) {
+							field.value = formSaveBtnrData[field.name];
+						}
+					}
+				}
+			};
+
+			var displayStatus = function (status) {
+				status.innerHTML = sessionStorage.getItem(formSaveBtnrID + '-formSaveBtnrMessage');
+				sessionStorage.removeItem(formSaveBtnrID + '-formSaveBtnrMessage');
+			};
+
+			// EVENTS, LISTENERS, AND INITS
+
+			// Populate form with data from localStorage
+			[].forEach.call(formFields, function (field) {
+				populateField(field);
+			});
+
+			// If page was reloaded and delete success message exists, display it
+			[].forEach.call(formStatus, function (status) {
+				displayStatus(status);
+			});
+
+		};
+
+		// EVENTS, LISTENERS, AND INITS
+
+		// Save form data
+		[].forEach.call(formSaveBtn, function (btn) {
+			btn.addEventListener('click', function(e) {
+				e.preventDefault();
+				saveForm(btn);
+			}, false);
+		});
+
+		// Delete form data
+		[].forEach.call(formRemoveBtn, function (btn) {
+			btn.addEventListener('click', function(e) {
+				e.preventDefault();
+				deleteForm(btn);
 			}, false);
 		});
 
 		// Get form data on page load
 		[].forEach.call(forms, function (form) {
-
-			// Variables
-			var formSaverID = form.id === null || form.id === '' ? 'formsaver-' + document.URL : 'formsaver-' + form.id;
-			var formSaverData = JSON.parse( localStorage.getItem(formSaverID) );
-			var formFields = form.elements;
-			var formStatus = form.querySelectorAll('.form-status');
-
-			// Populate form with data from localStorage
-			[].forEach.call(formFields, function (field) {
-				if ( formSaverData !== null ) {
-					if ( field.type == 'radio' || field.type == 'checkbox' ) {
-						if ( formSaverData[field.name + field.value] == 'on' ) {
-							field.checked = true;
-						}
-					} else if ( field.type != 'hidden' && field.type != 'submit' ) {
-						if ( formSaverData[field.name] !== null && formSaverData[field.name] !== undefined ) {
-							field.value = formSaverData[field.name];
-						}
-					}
-				}
-			});
-
-			// If page was reloaded and delete success message exists, display it
-			[].forEach.call(formStatus, function (status) {
-				status.innerHTML = sessionStorage.getItem(formSaverID + '-formSaverMessage');
-				sessionStorage.removeItem(formSaverID + '-formSaverMessage');
-			});
-
+			loadForm(form);
 		});
 
 	}
